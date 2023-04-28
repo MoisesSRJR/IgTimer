37df00a6ac4d09f67d05c2b8c0d0847d740121c9
MoisesSRJR <moises.sampaiojr@outlook.com>
2023-04-27 23:48:21 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,11 @@
-import { Header } from "./components/Header/Index";
-import { Post } from "./components/Post";
-import { Sidebar } from "./components/Sidebar/Index";
 
 import "./styles/global.css";
 
-const posts = [
-    {
-        id: 1,
-        author: {
-            avatarUrl: 'https://github.com/MoisesSRJR.png',
-            name: 'Moisés junior',
-            role: 'Dev front-end Pleno'
-        },
-        content: [
-            { type: 'paragraph', content: 'Fala Amigos 👋' },
-            { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-            { type: 'link', content: 'jane.design/doctorcare' },
-        ],
-        publishedAt: new Date('2023-03-29 20:00:00'),
-    },
-    {
-        id: 2,
-        author: {
-            avatarUrl: 'https://github.com/maykbrito.png',
-            name: 'Mayk Brito',
-            role: 'Educator @Rocketseat'
-        },
-        content: [
-            { type: 'link', content: 'jane.design/doctorcare' },
-        ],
-        publishedAt: new Date('2023-03-28 10:00:00'),
-    },
-];
 
 function App() {
     return (
         <>
-            <Header />
-            <div className="wrapper">
-                <Sidebar />
-                <main>
-                    {posts.map(post => {
-                        return (
-                            <Post
-                                key={post.id}
-                                image={post.author.avatarUrl}
-                                name={post.author.name}
-                                role={post.author.role}
-                                date={post.publishedAt}
-                                _content={post.content}
-                            />
-                        )
-                    })}
-                </main>
-            </div>
+
         </>
     );
 }
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,15 +1,23 @@
 const colors = {
-    green200: '#00875f',
-    green100: '#00b373',
-    gray800: '#121214',
-    gray700: '#202024',
-    gray600: '#323238',
-    gray500: '#7c7c8A',
-    gray400: '#8d8d99',
-    gray300: '#c4c4cc',
-    gray200: '#e1e1e6',
-    gray100: '#ffffff',
-    red: '#f75a68'
-  };
-  
-  export default colors;
+   'white': '#FFF',
+
+   'gray-100': '#E1E1E6',
+   'gray-300': '#C4C4CC',
+   'gray-400': '#8D8D99',
+   'gray-500': '#7C7C8A',
+   'gray-600': '#323238',
+   'gray-700': '#29292E',
+   'gray-800': '#202024',
+   'gray-900': '#121214',
+
+   'green-300': '#00B37E',
+   'green-500': '#00875F',
+   'green-700': '#015F43',
+
+   'red-500': '#AB222E',
+   'red-700': '#7A1921',
+
+   'yellow-500': '#FBA94C',
+};
+
+export default colors;
