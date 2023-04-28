0f6413f779ad1f9fa01018bb77c2be9080d7d038
MoisesSRJR <moises.sampaiojr@outlook.com>
2023-04-28 00:55:21 -0300

    added eslint and themes

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,10 @@
+// este arquivo serve somente para salvar definições de tipo typescript
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+// criando um tipagem para o módulo styped components, apenas adicionando ao que já tem.
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-
-import "./styles/global.css";
-
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 function App() {
-    return (
-        <>
-
-        </>
-    );
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>teste</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
