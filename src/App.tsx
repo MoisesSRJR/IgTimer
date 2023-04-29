38f519292ed3ede339eec40355a533d30dce2dec
MoisesSRJR <moises.sampaiojr@outlook.com>
2023-04-28 23:00:22 -0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 
 function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <h1>teste</h1>
-      <GlobalStyle />
-    </ThemeProvider>
-  )
+   return (
+      <ThemeProvider theme={defaultTheme}>
+         <BrowserRouter >
+            <Router />
+         </BrowserRouter>
+         <GlobalStyle />
+      </ThemeProvider>
+   )
 }
 
 export default App
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+   return (
+      <>
+         <h1>terste</h1>
+         <h3>teste</h3>
+      </>
+   )
+}
